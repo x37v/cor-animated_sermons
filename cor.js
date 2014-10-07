de0998898cff57ad63c0d3074e830739ff8c26de
Alex Norman <alexn@pure-imagination.com>
2014-10-07 10:06:29 -0700

    retabbed and made the fx interval more coarse

--- cor.js
+++ cor.js
@@ -1,3 +1,4 @@
+//vi:sw=2 ts=2 expandtab
 var slogans = ["Praise the mutant!", "Rejoice in error!", "Embrace futility!", "Beware of sinister progs!", "Take up the two 8 ways!", "Use the ninth position wisely!"];
 var colors = ['rgb(255, 0, 255)', 'rgb(0, 0, 255)', 'rgb(0, 255, 0)', 'rgb(255, 255, 0)'];
 var typed_page_timeout = 5000;
@@ -5,81 +6,82 @@ var paragrah_delay = 500;
 
 var text_attrs = [
   {
-		index:0,
-		interval: 500,
-			fade: 400,
-		colors: ['rgb(255, 0, 255)', 'rgb(0, 0, 255)', 'rgb(0, 255, 0)', 'rgb(255, 255, 255)']
-	},
+    index:0,
+    interval: 500,
+      fade: 400,
+    colors: ['rgb(255, 0, 255)', 'rgb(0, 0, 255)', 'rgb(0, 255, 0)', 'rgb(255, 255, 255)']
+  },
   {
-		index:0,
-		interval: 100,
-		fade: 90,
-		colors: ['rgb(255, 0, 255)', 'rgb(0, 0, 255)', 'rgb(0, 255, 0)', 'rgb(255, 255, 0)']
-	},
+    index:0,
+    interval: 100,
+    fade: 90,
+    colors: ['rgb(255, 0, 255)', 'rgb(0, 0, 255)', 'rgb(0, 255, 0)', 'rgb(255, 255, 0)']
+  },
   {
-		index:0,
-		interval: 200,
-		fade: 150,
-		colors: ['rgb(255, 0, 0)', 'rgb(255, 255, 255)']
-	}
+    index:0,
+    interval: 200,
+    fade: 150,
+    colors: ['rgb(255, 0, 0)', 'rgb(255, 255, 255)']
+  }
 ];
 
 function pageInit(){
-	window.setInterval(rotateBorderPallette, 200);
-	window.setInterval(rotateSlogans, 5000);
+  window.setInterval(rotateBorderPallette, 200);
+  window.setInterval(rotateSlogans, 5000);
 }
 
 function animatedSermonInit() {
-	jQuery.each(text_attrs, function(index, value) {
-		window.setInterval(function(){ pulseText(index); }, value.interval);
-	});
-	window.setInterval(function() {
-		$('html, body').animate({scrollTop:$(document).height()}, 'fast');
-	}, 500);
+  jQuery.fx.interval = 50;
+  jQuery.each(text_attrs, function(index, value) {
+    window.setInterval(function(){ pulseText(index); }, value.interval);
+  });
+  window.setInterval(function() {
+    $('html, body').animate({scrollTop:$(document).height()}, 'fast');
+  }, 500);
 }
 
 function rotateBorderPallette(){
-	nextColorForId('#border1');
-	nextColorForId('#border2');
-	nextColorForId('#border3');
-	nextColorForId('#border4');
+  nextColorForId('#border1');
+  nextColorForId('#border2');
+  nextColorForId('#border3');
+  nextColorForId('#border4');
 }
 
 function nextColorForId(itemId){
-	currentColor = $(itemId).css('border-color');
-	if (currentColor == "")
-		currentColor = $(itemId).css('border-left-color');
-	next = nextColor(currentColor);
-	$(itemId).css('border-color', next);
+  currentColor = $(itemId).css('border-color');
+  if (currentColor == "")
+    currentColor = $(itemId).css('border-left-color');
+  next = nextColor(currentColor);
+  $(itemId).css('border-color', next);
 }
 
 function nextColor(current){
-	for(i=0; i < colors.length; i++){
-		nextIndex = i+1;
-		if(nextIndex >= colors.length){
-			nextIndex = 0;
-		}
-		if(colors[i] == current){
-			return colors[nextIndex];
-		}
-	}
-	return colors[0];
+  for(i=0; i < colors.length; i++){
+    nextIndex = i+1;
+    if(nextIndex >= colors.length){
+      nextIndex = 0;
+    }
+    if(colors[i] == current){
+      return colors[nextIndex];
+    }
+  }
+  return colors[0];
 }
 
 function rotateSlogans()
 {
-	var slogan = slogans[Math.floor(Math.random() * slogans.length)];
-	$('#slogans').text(slogan);
+  var slogan = slogans[Math.floor(Math.random() * slogans.length)];
+  $('#slogans').text(slogan);
 }
 
 function pulseText(group)
 {
-	var colors = text_attrs[group].colors;
-	var fade = text_attrs[group].fade;
-	var cindex = text_attrs[group].index;
-	text_attrs[group].index = (cindex + 1) % colors.length;
-	var c = colors[cindex];
-	$(".rt" + String(group)).animate({color:c}, fade);
+  var colors = text_attrs[group].colors;
+  var fade = text_attrs[group].fade;
+  var cindex = text_attrs[group].index;
+  text_attrs[group].index = (cindex + 1) % colors.length;
+  var c = colors[cindex];
+  $(".rt" + String(group)).animate({color:c}, fade);
 }
 
 //textarray is actually just one long string
@@ -93,68 +95,68 @@ function pulseText(group)
 //
 //all content is appended to the html element specified by append_element
 function typetext(append_element, textarray, donecallback) {
-	var shit = textarray.split("|");
-	var f = function(index) {
-		if (index < shit.length)
-			type(append_element, shit[index], function() { f(index + 1); } );
-		else {
-			donecallback();
-		}
-	};
-	f(0);
+  var shit = textarray.split("|");
+  var f = function(index) {
+    if (index < shit.length)
+      type(append_element, shit[index], function() { f(index + 1); } );
+    else {
+      donecallback();
+    }
+  };
+  f(0);
 }
 
 function type(append_element, text, donefunc) {
-	var group = 0;
-	var element = document.createElement("span");
-	var re = /^(\d)=/;
-	var br = false;
-	if (/BR/.exec(text)) {
-		$(append_element).append("<br><br>");
-		text = text.substr(2);
-		br = true;
-	}
+  var group = 0;
+  var element = document.createElement("span");
+  var re = /^(\d)=/;
+  var br = false;
+  if (/BR/.exec(text)) {
+    $(append_element).append("<br><br>");
+    text = text.substr(2);
+    br = true;
+  }
 
-	var test = re.exec(text);
-	if (test) {
-		group = test[1];
-		text = text.substr(2);
-	}
-	if (/XXX/.exec(text)) {
-		var x = function() {
-			$(append_element).empty();
-			donefunc();
-		};
-		setTimeout(x, typed_page_timeout);
-	} else {
-		if (br && !/\s*\^/.exec(text)) {
-			text = "^" + paragrah_delay + text;
-		}
-		$(element).addClass("rt" + String(group));
-		//$(element).data("group", group);
-		$(append_element).append(element);
-		$(element).typed({
-			strings: [text],
-			typeSpeed: 0,
-			showCursor: false,
-			callback: donefunc
-		});
-	}
+  var test = re.exec(text);
+  if (test) {
+    group = test[1];
+    text = text.substr(2);
+  }
+  if (/XXX/.exec(text)) {
+    var x = function() {
+      $(append_element).empty();
+      donefunc();
+    };
+    setTimeout(x, typed_page_timeout);
+  } else {
+    if (br && !/\s*\^/.exec(text)) {
+      text = "^" + paragrah_delay + text;
+    }
+    $(element).addClass("rt" + String(group));
+    //$(element).data("group", group);
+    $(append_element).append(element);
+    $(element).typed({
+      strings: [text],
+      typeSpeed: 0,
+      showCursor: false,
+      callback: donefunc
+    });
+  }
 }
 
 //slop overflow:
 //http://stackoverflow.com/questions/19491336/get-url-parameter-jquery
 function getUrlParameter(sParam)
 {
-	var sPageURL = window.location.search.substring(1);
-	var sURLVariables = sPageURL.split('&');
-	for (var i = 0; i < sURLVariables.length; i++) 
-	{
-		var sParameterName = sURLVariables[i].split('=');
-		if (sParameterName[0] == sParam) 
-		{
-			return sParameterName[1];
-		}
-	}
-	return null;
+  var sPageURL = window.location.search.substring(1);
+  var sURLVariables = sPageURL.split('&');
+  for (var i = 0; i < sURLVariables.length; i++) 
+  {
+    var sParameterName = sURLVariables[i].split('=');
+    if (sParameterName[0] == sParam) 
+    {
+      return sParameterName[1];
+    }
+  }
+  return null;
 }
